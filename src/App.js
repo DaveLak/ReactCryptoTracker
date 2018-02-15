8c84c365cc51f11c04bd2e113bc315f866a7c523
David Lakin <dal.scny@gmail.com>
2018-02-15 12:02:52 -0500

    Eject from create-react-app; Delete header; Update README.md
    Ejecting from create react app to give us a little more control over
    the build system when it's needed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import 'normalize.css';
-import Header from './components/presentational/Header/Header';
 import CardHolder from './components/presentational/CardHolder/CardHolder';
 import CoinListContainer from './components/container/CoinListContainer';
 import './App.css';
@@ -9,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <CardHolder>
           <CoinListContainer />
         </CardHolder>
