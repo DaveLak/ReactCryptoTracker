b8320487c44dd101b8e0c8a74e76d4425c1397f1
David Lakin <dal.scny@gmail.com>
2018-02-21 17:12:30 -0500

    Add a default icon and move svgs to assets dir.

--- src/components/presentation/Card/Card.js
+++ src/components/presentation/Card/Card.js
@@ -1,14 +1,16 @@
 import React from "react";
+import placeholder from "../../../assets/images/placeholder.svg";
 import "./Card.css";
 
 const Card = ({ imgSrc, name, symbol, price, displayCurrency }) => (
   <div className="Card">
-    <img src={imgSrc} alt={`${name} logo`} />
     <h3>{name}</h3>
+    <img src={imgSrc !== "" ? imgSrc : placeholder} alt={`${name} logo`} />
     <p>{symbol}</p>
     <p>
       {price} {displayCurrency}
     </p>
   </div>
 );
+
 export default Card;
